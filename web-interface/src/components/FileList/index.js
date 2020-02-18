4ea91aaf8761c493a0df68b21e73c206605de0c0
casserekevin <kevin_cassere13@hotmail.com>
2020-02-17 23:34:11 -0300

    List of files in front-end

--- web-interface/src/components/FileList/index.js
+++ web-interface/src/components/FileList/index.js
@@ -10,38 +10,44 @@ const divIcons = {
     alignItems: 'center'
 }
 
-const FileList = () => (
+const FileList = ({ files }) => (
         <Container>
-            <li>
-                <FileInfo>
-                    <Preview src='http://localhost:3000/files/08fdac4722e0d1a21e383bad47262b9e-profile.png' />
-                    <div>
-                        <strong>profile.png</strong>
-                        <span>64kb <button onClick={ () => {} }>Excluir</button></span>
+            { files.map((uploadedFile) => (
+                <li>
+                    <FileInfo>
+                        <Preview src={ uploadedFile.preview } />
+                        <div>
+                            <strong>{ uploadedFile.name }</strong>
+                            <span>{ uploadedFile.readableSize } <button onClick={ () => {} }>Excluir</button></span>
+                        </div>
+                    </FileInfo>
+
+                    <div style={divIcons} >
+                        { !uploadedFile.uploaded && !uploadedFile.error && (
+                            <CircularProgressbar
+                                styles={{
+                                    root: { width: 24 },
+                                    path: { stroke: '#7159c1'}
+                                }}
+                                strokeWidth={10}
+                                value={uploadedFile.progress}
+                            />
+                        ) }
+                        { uploadedFile.url && (
+                            <a 
+                                href='http://localhost:3000/files/08fdac4722e0d1a21e383bad47262b9e-profile.png'
+                                target='_blank'
+                                rel='noopener noreferrer'
+                            >
+                                <MdLink style={{ marginRight: 8 }} size={24} color='#222'/>
+                            </a> 
+                        ) }   
+
+                        { uploadedFile.uploaded && <MdCheckCircle size={24} color='#78e5d5'/> }
+                        { uploadedFile.error && <MdError size={24} color='#e57878'/> }
                     </div>
-                </FileInfo>
-
-                <div style={divIcons} >
-                    <CircularProgressbar
-                        styles={{
-                            root: { width: 24 },
-                            path: { stroke: '#7159c1'}
-                        }}
-                        strokeWidth={10}
-                        value={60}
-                    />
-                    <a 
-                        href='http://localhost:3000/files/08fdac4722e0d1a21e383bad47262b9e-profile.png'
-                        target='_blank'
-                        rel='noopener noreferrer'
-                    >
-                        <MdLink style={{ marginRight: 8 }} size={24} color='#222'/>
-                    </a>    
-
-                    <MdCheckCircle size={24} color='#78e5d5'/>
-                    <MdError size={24} color='#e57878'/>
-                </div>
-            </li>
+                </li>
+            )) }
         </Container>
     )
 
